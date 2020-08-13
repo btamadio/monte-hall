c0ba64494ef1a47b9037ef9ec4fc499a8bd02f9a
Brian Amadio <brian.amadio@stitchfix.com>
2020-08-13 12:39:27 -0700

    add images and fix layout

--- src/cljs/monte_hall/views.cljs
+++ src/cljs/monte_hall/views.cljs
@@ -8,23 +8,17 @@
 (defn door-image
   [open? prize?]
   (case open?
-    false "http://www.sbrusticdoors.com/wp-content/uploads/2011/11/double-big-arch-rustic-door-200x300.jpg"
-    true (if prize?
-           "https://i.giphy.com/media/l1rrKjeprm2VsbCc1w/giphy.gif"
-           "https://thriftyhomesteader.com/wp-content/uploads/2020/02/1.pin_-2-200x300.png")))
+    false "images/closed-door.jpg"
+    true (if prize? "images/prize.gif" "images/goat.png")))
 
-(defn door-class
-  [open? selected?]
-  (if open? "door-open"
-      (if selected? "door-selected" "door"))
-)
 
 (defn door
   [{:keys [id open? prize? selected?]}]
   [:div.card.has-text-centered {:on-click (when (not open?) #(dispatch [::events/set-selected-door id]))}
    [:div.card-content
     [:img.door {:src (door-image open? prize?)
-                :class (door-class open? selected?)}]]])
+                :height (if open? 300 200)
+                :width (if open? 300 200)}]]])
 
 (defn doors
   []
@@ -36,9 +30,9 @@
 (defn select-button
   [on-click]
   (if-let [door-id @(subscribe [::subs/selected-door])]
-    [:button.button.is-primary.is-medium {:class "select-door"
-              :on-click on-click}
-     "Choose Door " (inc door-id)]))
+    [:button.button.is-primary.is-medium {:on-click on-click}
+     "Choose Door " (inc door-id)]
+    [:button.button.is-medium {:disabled true} "Choose a door"]))
 
 (defn new-game-button
   []
@@ -70,20 +64,6 @@
      [:td (:stayed-won conf-matrix)]
      [:td (:stayed-lost conf-matrix)]]]])
 
-(defn nav-bar
-  []
-  [:nav.navbar.is-fixed-top
-   [:div.navbar-brand
-    [:a.navbar-item
-     [:h1.title.is-1 "Monte Hall"]]]
-   [:div.navbar-menu
-    [:div.navbar-start
-     [:a.navbar-item "Play"]]
-    [:div.navbar-end
-     [:a.navbar-item {:href "https://github.com/btamadio/monte-hall" :target "_blank"}
-      [:span.icon.is-large
-       [:i.fa.fa-2x.fa-github]]]]]])
-
 (defn notif-text
   [game-stage game-result]
   (case game-stage
@@ -97,13 +77,11 @@
         game-stage @(subscribe [::subs/game-stage])
         game-result @(subscribe [::subs/game-result])
         conf-matrix @(subscribe [::subs/confusion-matrix])]
-    [:div.container
-     [:div.block [nav-bar]]
-     [:div.block
-      [:div.game
-       [:div.notification (notif-text game-stage game-result)]
-       [doors]
-       (when (= game-mode :play) [play-button])]]
+    [:div.block [:div.block
+                 [:div.game
+                  [:div.notification (notif-text game-stage game-result)]
+                  [doors]
+                  (when (= game-mode :play) [play-button])]]
      [:div.block
       [:div.box
        [:div.content.is-medium
